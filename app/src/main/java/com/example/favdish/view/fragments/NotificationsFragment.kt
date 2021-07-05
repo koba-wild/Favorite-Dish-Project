223d4f5fb8d6b6a04ba0062c6b7ca4f290ab748b
Эвгеній  Кобахідзе <seraphjackson@gmail.com>
2021-07-05 16:04:23 +0300

    -wild First commit

---
+++ app/src/main/java/com/example/favdish/view/fragments/NotificationsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.favdish.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.favdish.R
+import com.example.favdish.viewmodel.RandomDishFragment
+
+class NotificationsFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: RandomDishFragment
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+                ViewModelProvider(this).get(RandomDishFragment::class.java)
+        val root = inflater.inflate(R.layout.fragment_random_dish, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
